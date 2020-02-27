c27a04dffe520e76474276684f9c34fce011db02
Ankit Sharma <45990579+ankitx007@users.noreply.github.com>
2020-02-27 16:12:49 +0530

    Update 1
    No more scans

--- whatsapp.py
+++ whatsapp.py
@@ -12,3 +12,23 @@ for i in range(count):
     msg_box.send_keys(msg)
     button = driver.find_element_by_class_name("_3M-N-")
     button.click()
+while(1):
+    print('Do you want to send message again?')
+    k = input("If yes enter 'Y' or 'y' and for no enter 'N' or 'n': ")
+    if k == 'y' or k == 'Y':
+        name = input('Enter the name of the user or group: ')
+        msg = input('Enter the message: ')
+        count = int(input('Enter the count: '))
+        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+        user.click()
+        msg_box = driver.find_element_by_class_name("_13mgZ")
+        for i in range(count):
+            msg_box.send_keys(msg)
+            button = driver.find_element_by_class_name("_3M-N-")
+            button.click()
+    elif(k=='n' or k=='N'):
+        print('Thank you for using.')
+        break
+    else:
+        print('Invalid Input')
+        break
