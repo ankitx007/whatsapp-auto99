43b246d5bd0c6413081d0e3fcf8b5daf052aceb6
Ankit Sharma <7ankit4@gmail.com>
2021-06-10 22:41:12 +0530

    Update whatsapp.py
    A simple function change

--- whatsapp.py
+++ whatsapp.py
@@ -1,40 +1,44 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get('https://web.whatsapp.com/')
+def whatsapp(name,msg,count):
+    driver = webdriver.Chrome()
+    driver.get('https://web.whatsapp.com/')
+    input('Press any key after scanning QR code.')
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+    msg_box = driver.find_element_by_class_name("_1Plpp")
+    for i in range(count):
+        print(i)
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_class_name("_35EW6")
+        button.click()
+    while(1):
+        print('Do you want to send message again?')
+        k = input("If yes enter 'Y' or 'y' and for no enter 'N' or 'n': ")
+        if k == 'y' or k == 'Y':
+            name = input('Enter the name of the user or group: ')
+            msg = input('Enter the message: ')
+            count = int(input('Enter the count: '))
+            user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+            user.click()
+            msg_box = driver.find_element_by_class_name("_1Plpp")
+            for i in range(count):
+                print(i)
+                msg_box.send_keys(msg)
+                button = driver.find_element_by_class_name("_35EW6")
+                button.click()
+        elif(k=='n' or k=='N'):
+            log_out = driver.find_elements_by_class_name('rAUz7')
+            log_out[2].click()
+            pp = driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[3]/span/div/ul/li[6]/div')
+            pp.click()
+            print('You have been logged out. Thank you for using.')
+            break
+        else:
+            print('Invalid Input')
+            break
+
+
 name = input('Enter the name of the user or group: ')
 msg = input('Enter the message: ')
 count = int(input('Enter the count: '))
-input('Press any key after scanning QR code.')
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
-msg_box = driver.find_element_by_class_name("_13mgZ")
-for i in range(count):
-    print(i)
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name("_3M-N-")
-    button.click()
-while(1):
-    print('Do you want to send message again?')
-    k = input("If yes enter 'Y' or 'y' and for no enter 'N' or 'n': ")
-    if k == 'y' or k == 'Y':
-        name = input('Enter the name of the user or group: ')
-        msg = input('Enter the message: ')
-        count = int(input('Enter the count: '))
-        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-        user.click()
-        msg_box = driver.find_element_by_class_name("_13mgZ")
-        for i in range(count):
-            print(i)
-            msg_box.send_keys(msg)
-            button = driver.find_element_by_class_name("_3M-N-")
-            button.click()
-    elif(k=='n' or k=='N'):
-        print('Thank you for using.')
-        log_out = driver.find_elements_by_class_name('_3j8Pd')
-        log_out[2].click()
-        pp = driver.find_element_by_class_name('_3zy-4 Sl-9e')
-        pp[0].click()
-        break
-    else:
-        print('Invalid Input')
-        break
+whatsapp(name,msg,count)
