832c954b58b1c5144ab7431e3d39134c2289dfbb
Ankit Sharma <7ankit4@gmail.com>
2021-06-10 23:49:27 +0530

    Update whatsapp.py
    Updated to new whatsapp

--- whatsapp.py
+++ whatsapp.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 def whatsapp(name,msg,count):
+    driver = webdriver.Chrome()
+    driver.get('https://web.whatsapp.com/')
+    input('Press any key after scanning QR code.')
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     user.click()
-    msg_box = driver.find_element_by_class_name("_1Plpp")
+    msg_box = driver.find_element_by_class_name("_2A8P4")
     for i in range(count):
         print(i)
         msg_box.send_keys(msg)
-        button = driver.find_element_by_class_name("_35EW6")
+        button = driver.find_element_by_class_name("_1E0Oz")
         button.click()
     while(1):
         print('Do you want to send message again?')
@@ -17,16 +20,16 @@ def whatsapp(name,msg,count):
             count = int(input('Enter the count: '))
             user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
             user.click()
-            msg_box = driver.find_element_by_class_name("_1Plpp")
+            msg_box = driver.find_element_by_class_name("_2A8P4")
             for i in range(count):
                 print(i)
                 msg_box.send_keys(msg)
-                button = driver.find_element_by_class_name("_35EW6")
+                button = driver.find_element_by_class_name("_1E0Oz")
                 button.click()
         elif(k=='n' or k=='N'):
-            log_out = driver.find_elements_by_class_name('rAUz7')
+            log_out = driver.find_elements_by_class_name('_1XaX-')
             log_out[2].click()
-            pp = driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[3]/span/div/ul/li[6]/div')
+            pp = driver.find_element_by_xpath('//span[aria-label="Log out"]')
             pp.click()
             print('You have been logged out. Thank you for using.')
             break
@@ -38,7 +41,4 @@ def whatsapp(name,msg,count):
 name = input('Enter the name of the user or group: ')
 msg = input('Enter the message: ')
 count = int(input('Enter the count: '))
-driver = webdriver.Chrome()
-driver.get('https://web.whatsapp.com/')
-input('Press any key after scanning QR code.')
 whatsapp(name,msg,count)
